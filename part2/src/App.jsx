715ff86e84ae8b3740d0cf024f8f23c094d5ecc1
Dairon <daironc78@gmail.com>
2024-12-23 19:46:36 -0500

    Consumo de servicio consulta

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import Contacts from './components/contacts/Contacts'
-import AddContacts from './components/addContact/AddContacts'
-import SearchContact from './components/searchContact/searchContact'
-import './App.css'
+import { useState, useEffect } from "react";
+import Contacts from "./components/contacts/Contacts";
+import AddContacts from "./components/addContact/AddContacts";
+import SearchContact from "./components/searchContact/searchContact";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [filterPerson, setFilterPersons] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [filterPerson, setFilterPersons] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data);
+        console.log("PromiseContacts", response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   const addPerson = (event) => {
-    console.log('addContact', event);
+    console.log("addContact", event);
 
-    if (persons.some(person => person.name.toLowerCase() === event.name.trim().toLowerCase())) {
-      alert(`${event.name} is already added to phonebook`)
-      return
+    if (
+      persons.some(
+        (person) =>
+          person.name.toLowerCase() === event.name.trim().toLowerCase()
+      )
+    ) {
+      alert(`${event.name} is already added to phonebook`);
+      return;
     }
 
     const personObject = {
       id: persons.length + 1,
       name: event.name.trim(),
-      number: event.number.trim()
-    }
+      number: event.number.trim(),
+    };
 
-    setPersons(persons.concat(personObject))
-  }
+    setPersons(persons.concat(personObject));
+  };
 
   const personsToShow = (event) => {
-    setFilterPersons(event)
-  }
+    setFilterPersons(event);
+  };
 
   return (
     <div>
@@ -37,7 +55,7 @@ const App = () => {
       <hr />
       <Contacts persons={persons} filterPerson={filterPerson} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
